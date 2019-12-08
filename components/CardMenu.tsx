1b660fe0ef1ebbf4c1dee8ebd008d904625a5933
Rached Mejri <r.mejri74100@gmail.com>
2019-12-08 12:05:41 +0100

    feat(card menu): add card menu component

--- components/CardMenu.tsx
+++ components/CardMenu.tsx
@@ -1,35 +1,41 @@
 import React from "react"
-import {Text, Card, Layout} from "@ui-kitten/components"
+import {Text} from "@ui-kitten/components"
 import {StyleSheet} from "react-native"
-import {TouchableHighlight, Image} from "react-native"
+import {TouchableOpacity, ImageBackground} from "react-native"
 
 const styles = StyleSheet.create({
-    layout: {
-        marginTop: 5,
-        marginVertical: 7,
-        flex: 1
+    image: {
+        flex: 1, 
+        marginHorizontal: 15,
+        marginVertical: 10
     },
 
-    title: {
-        
+    imageText: {
+        backgroundColor: "#00E096",
+        paddingBottom: 12,
+        paddingLeft: 10,
+        paddingTop: 7,
+        opacity: 0.8,
+        borderRadius: 10
     }
-});
+})
 
 interface Props {
     title: String,
-    subtitle: String
+    imageUrl: String
 }
 
-const CardMenu: React.FC<Props> = (props) => {
+const CardMenu: React.FC<Props> = (props: Props) => {
     return (
-        <Layout style={styles.layout}>
-            <TouchableHighlight>
-                <Card>
-                    <Text category="h1" style={styles.title}>{props.title}</Text>
-                    <Text category="s2">{props.subtitle}</Text>
-                </Card>
-            </TouchableHighlight>
-        </Layout>
+        <TouchableOpacity style={styles.image}>
+            <ImageBackground
+                source={{uri: props.imageUrl}} 
+                imageStyle={{borderRadius: 10}}
+                style={{flex: 1}}
+            >
+                <Text category="h4" appearance="alternative" style={styles.imageText}>{props.title}</Text>
+            </ImageBackground>
+        </TouchableOpacity>
     );
 }
 
