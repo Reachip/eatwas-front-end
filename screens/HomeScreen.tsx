8606aa9f736db3f7f37d7d571e4dc5774dfa23a0
Rached Mejri <r.mejri74100@gmail.com>
2019-12-08 12:06:34 +0100

    feat(user menu): user menu

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -15,22 +15,15 @@ export default class HomeScreen extends Component<Props, State> {
         return( 
             <Layout style={{flex: 1}}>
                 <CardMenu 
-                    title="Consulter ma progression" 
-                    subtitle="Consulter votre progression via vos données personnelles"
-                >    
-                </CardMenu>
-
+                    imageUrl="https://images.unsplash.com/photo-1498837167922-ddd27525d352?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" 
+                    title="Mon profil"
+                ></CardMenu>
                 <CardMenu 
-                    title="Mes suggestions" 
-                    subtitle="Recevez des suggestions qui prennent en comptre vos objectifs et l'environnement"
-                >    
-                </CardMenu>
-
+                    imageUrl="https://images.unsplash.com/photo-1466978913421-dad2ebd01d17?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=667&q=80" 
+                    title="Mes suggestions"></CardMenu>
                 <CardMenu 
-                    title="Mon bracelet connecté" 
-                    subtitle="Connecter le bracelet connecté à l'application afin d'en savoir plus sur votre dépense journalière"
-                >    
-                </CardMenu>
+                    imageUrl="https://images.unsplash.com/photo-1501959915551-4e8d30928317?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80" 
+                    title="Mon bracelet connecté"></CardMenu>
             </Layout>
         );
     }
