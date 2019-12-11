a17f0bad0c459c74ab057e943dcba0c0ba65c2ba
Rached Mejri <r.mejri74100@gmail.com>
2019-12-11 19:43:35 +0100

    feat(screen) add some screens

---
+++ screens/SuggestionScreen.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import {Component} from 'react'
+import {Layout, Text} from "@ui-kitten/components"
+
+interface Props {}
+
+interface State {}
+
+export default class SuggestionScreen extends Component<Props, State>  {
+    constructor(props: Props) {
+        super(props)
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <Layout style={{flex: 1}}>
+                <Text category="h2">Suggestion screen</Text>
+            </Layout>
+        );
+    }
+}
