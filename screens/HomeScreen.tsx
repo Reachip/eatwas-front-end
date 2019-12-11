6d5727ece1e355656234b54c1d125441b81f315a
Rached Mejri <r.mejri74100@gmail.com>
2019-12-11 19:45:51 +0100

    feat(home screen) add the three sections

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -12,18 +12,25 @@ export default class HomeScreen extends Component<Props, State> {
     }
 
     render() {
+        const {navigate} = this.props.navigation;
+
         return( 
             <Layout style={{flex: 1}}>
                 <CardMenu 
                     imageUrl="https://images.unsplash.com/photo-1498837167922-ddd27525d352?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" 
                     title="Mon profil"
+                    switchTo={() => navigate("Profil")}
                 ></CardMenu>
                 <CardMenu 
                     imageUrl="https://images.unsplash.com/photo-1466978913421-dad2ebd01d17?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=667&q=80" 
-                    title="Mes suggestions"></CardMenu>
+                    title="Mes suggestions"
+                    switchTo={() => navigate("Suggestion")}
+                ></CardMenu>
                 <CardMenu 
                     imageUrl="https://images.unsplash.com/photo-1501959915551-4e8d30928317?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80" 
-                    title="Mon bracelet connecté"></CardMenu>
+                    title="Mon bracelet connecté"
+                    switchTo={() => navigate("Bracelet")}
+                ></CardMenu>
             </Layout>
         );
     }
