7b2772660153a17d70f5e9bdd492b088ee34b3b9
Rached Mejri <r.mejri74100@gmail.com>
2019-12-15 13:30:43 +0100

    feat(BraceletScreen): add a menu

--- screens/BraceletScreen.jsx
+++ screens/BraceletScreen.jsx
@@ -1,17 +1,30 @@
 import React, {Component} from 'react'
-import {Layout, Text} from "@ui-kitten/components"
+import {Menu, Icon} from "@ui-kitten/components"
 
 export default class BraceletScreen extends Component {
     constructor(props) {
         super(props)
-        this.state = {}
+        this.state = {
+            menuData: [
+                { title: 'Me connecter au bracelet', icon: (style) => (<Icon {...style} name='link-outline'/>)},
+                { title: 'Me déconnecter du bracelet', disabled: true, icon: (style) => (<Icon {...style} name='minus-circle-outline'/>) },
+                { title: 'Configurer un bracelet', icon: (style) => (<Icon {...style} name='settings-2-outline'/>) },
+                { title: 'Envoyer mes données à EatWas', disabled: true, icon: (style) => (<Icon {...style} name='save-outline'/>) },
+            ],
+ 
+            selectedIndex: null
+        }
     }
 
     render() {
         return (
-            <Layout style={{flex: 1}}>
-                <Text category="h2">Bracelet screen</Text>
-            </Layout>
+            <Menu
+                appearance="default"
+                data={this.state.menuData}
+                selectedIndex={this.state.selectedIndex}
+                onSelect={(index) => this.setState({selectedIndex: index})}
+                style={{flex: 1}}
+            />
         );
     }
 }
